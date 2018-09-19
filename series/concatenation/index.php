a0bc0e46b20c84e54c9412e9103801b1f0828f0e
Mrdbachl <35881442+Mrdbachl@users.noreply.github.com>
2018-09-18 20:45:41 -0600

    Add files via upload

---
+++ series/concatenation/index.php
@@ -0,0 +1,14 @@
+<?php
+
+$day = 'Thursday';
+$date = 31;
+$year = 2014;
+$name = 'Chris';
+$mid = 'Anthony';
+$last = 'Rickenbach';
+
+echo 'My name is '.$name.' '.$mid.' '.$last.' ';
+
+echo "The date is <strong> $day $date $year </strong>";
+
+?>
