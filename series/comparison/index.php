ca392bec01284ee0bbe35f00b419b547838e1c7a
Mrdbachl <35881442+Mrdbachl@users.noreply.github.com>
2018-09-24 14:28:27 -0600

    Add files via upload

---
+++ series/comparison/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$number1 = 10;
+$number2 = 10;
+
+if ($number1<=$number2) {
+  echo 'Yes.';		
+} else {
+  echo 'No.';	
+}
+
+$password = 'Chris';
+
+if ($password=='Bob') {
+   echo 'Correct.';
+} else {
+	echo 'Incorrect.';
+}
+
+?>
