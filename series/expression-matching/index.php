da63177f4121acee971022f73da04e7b3a6939d6
Mrdbachl <35881442+Mrdbachl@users.noreply.github.com>
2018-10-07 14:09:31 -0600

    Add files via upload

--- series/expression-matching/index.php
+++ series/expression-matching/index.php
@@ -2,13 +2,21 @@
 $string = 'This is a string.';
 $var2 = 'This includes other words that may be used in the creation of blob programs.';
 
-if (preg_match('/blob/', $string, '/is/', $var2)) {
+if (preg_match('/blob/', $string)) {
 	
-	echo 'Match found.'; 
+	echo 'Match found.'.'<br>'; 
 } else {
-	echo 'Match not found.';
+	echo 'Match not found.'.'<br>';
 	
-	}	
+	
+	}
+
+if (preg_match('/blob/', $var2)) {
+	
+echo 'That\'s a match';
+} else {
+	echo ' No Match.';
+}	
 	
 
 
