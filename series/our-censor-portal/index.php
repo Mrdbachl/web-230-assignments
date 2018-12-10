669fbef443b25aa400a804de93ab705afba46afc
Mrdbachl <cr60722@ravens.racc.edu>
2018-12-10 15:57:53 -0700

    -m "started ocp project"

--- series/our-censor-portal/index.php
+++ series/our-censor-portal/index.php
@@ -1,10 +1,36 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
+<?php
 
-<body>
-</body>
-</html>
+include_once ('include/header.inc.php');
+
+$offset = 0;	
+
+if(isset($_POST['text'])) {
+	
+	$text = $_POST['text'];
+	
+	$search = 'Acme';
+	
+	$replace = 'Insights';
+	
+	$search_length = strlen($search);
+	
+		if(!empty($text)){
+			
+			while ($strpos = strpos($text, $search, $offset)) {
+				
+				$offset = $strpos + $search_length;
+				
+				$text = substr_replace($text, $replace, $strpos, $search_length);
+			}
+			
+		} else {
+			
+			echo 'Please fill in the text fields.';
+			
+		} else {
+			
+			$text = 'Please enter your communication here.';
+		}
+}
+	
+?>
