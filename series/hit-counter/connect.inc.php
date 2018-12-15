ba34abb76ebd8865e74fdfef31e83756d2ad86b4
Mrdbachl <cr60722@ravens.racc.edu>
2018-12-14 18:51:52 -0700

    committed unique ip hit counter

---
+++ series/hit-counter/connect.inc.php
@@ -0,0 +1,17 @@
+<?php
+
+$conn_error = 'Could not connect.';
+
+$mysqli_host = 'localhost';
+$mysqli_user = 'root';
+$mysqli_pass = '';
+$mysql_db = 'a_database';
+
+if (!$con = mysqli_connect($mysqli_host, $mysqli_user, $mysqli_pass, $mysql_db)) {
+	die($conn_error);
+	
+} else {
+	
+}
+
+?>
